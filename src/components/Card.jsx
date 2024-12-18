a37e16d31a8c3d1be2194e3a3cb6603dd9b07726
Lena Hauer <lena.hauer@me.com>
2024-12-18 21:37:26 +0100

    Added entry page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Card = ({ entry }) => {
   return (
     <div className="card bg-base-100 image-full w-96 shadow-xl">
@@ -8,7 +10,9 @@ const Card = ({ entry }) => {
         <h2 className="card-title">{entry.title}</h2>
         <p>{entry.date}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">See more</button>
+          <Link to={`/entry/${entry.id}`} className="btn btn-primary">
+            Read more
+          </Link>
         </div>
       </div>
     </div>
--- src/views/Entry.jsx
+++ src/views/Entry.jsx
@@ -1,9 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Post from "../components/Post";
 
 const Entry = () => {
+  console.log("Entry.jsx");
+
+  const { id } = useParams(); // Extract the entry ID from the URL
+  console.log("ID outside useEffect:", id);
+  const [entry, setEntry] = useState(null);
+
+  // Function to get the entry by ID from localStorage
+  const getPost = (id) => {
+    const entries = JSON.parse(localStorage.getItem("entries")) || [];
+    console.log("Entries in localStorage:", entries);
+    return entries.find((entry) => entry.id === id);
+  };
+
+  console.log(localStorage.getItem("entries"));
+
+  useEffect(() => {
+    console.log("ID from useParams:", id);
+    const fetchedEntry = getPost(id);
+    console.log("Fetched Entry:", fetchedEntry);
+    setEntry(fetchedEntry);
+  }, [id]);
+
   return (
     <div>
       <NavBar />
+      {entry ? (
+        <Post entry={entry} />
+      ) : (
+        <p className="text-center">Loading...</p>
+      )}
     </div>
   );
 };
